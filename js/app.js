8e51b37742303196a290aa7b5fa286259d1e2fd3
Vlad <vlad.m21924@gmail.com>
2021-03-01 16:36:58 +0200

    create geeting section

--- js/app.js
+++ js/app.js
@@ -8,3 +8,13 @@ headerBurger.addEventListener('click', function() {
 	headerNav.classList.toggle('open');
 	body.classList.toggle('lock');
 })
+
+function ibg(){
+	let ibg=document.querySelectorAll(".ibg");
+	for (var i = 0; i < ibg.length; i++) {
+		if(ibg[i].querySelector('img')) {
+			ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
+		}
+	}
+}
+ibg();
